e8479b21cc4cdb0938fdd7ce2322d8238da134fe
muzea <mr.muzea@gmail.com>
2025-03-12 04:59:57 +0000

    feat: 并发判题

--- app/scheduler.go
+++ app/scheduler.go
@@ -2,11 +2,53 @@ package app
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/Pigeon-Developer/pigeon-oj-judge/actuator"
 	"github.com/Pigeon-Developer/pigeon-oj-judge/solution"
 )
+
+// MaxConcurrentJudges defines the maximum number of concurrent judge operations
+var MaxConcurrentJudges = 4
+
+// WorkerPool manages a pool of workers for concurrent judgement
+type WorkerPool struct {
+	jobChan chan *solution.JudgeJob
+	wg      sync.WaitGroup
+}
+
+// NewWorkerPool creates a new worker pool with n workers
+func NewWorkerPool(n int) *WorkerPool {
+	return &WorkerPool{
+		jobChan: make(chan *solution.JudgeJob, n),
+	}
+}
+
+// Start starts the worker pool
+func (wp *WorkerPool) Start() {
+	for i := range MaxConcurrentJudges {
+		wp.wg.Add(1)
+		go func(workerID int) {
+			defer wp.wg.Done()
+			for job := range wp.jobChan {
+				result := actuator.JudgeUserSubmit(job)
+				job.UpdateResult(result)
+			}
+		}(i)
+	}
+}
+
+// Submit submits a job to the worker pool
+func (wp *WorkerPool) Submit(job *solution.JudgeJob) {
+	wp.jobChan <- job
+}
+
+// Close closes the worker pool
+func (wp *WorkerPool) Close() {
+	close(wp.jobChan)
+	wp.wg.Wait()
+}
 
 func fetchSolutionFromPool() (*solution.JudgeJob, error) {
 	var soluton *solution.Solution
@@ -25,16 +67,33 @@ func fetchSolutionFromPool() (*solution.JudgeJob, error) {
 }
 
 func RunLoop() {
-	for {
-		job, err := fetchSolutionFromPool()
-		if err == nil {
-			// 需要判题
-			result := actuator.JudgeUserSubmit(job)
+	// Create a semaphore channel with capacity MaxConcurrentJudges
+	semaphore := make(chan struct{}, MaxConcurrentJudges)
 
-			job.UpdateResult(result)
-		} else {
-			fmt.Println(err)
+	for {
+		// Try to acquire a semaphore slot
+		select {
+		case semaphore <- struct{}{}: // Successfully acquired a slot
+			// Fetch and process a new job only when we have capacity
+			go func() {
+				defer func() {
+					// Release the semaphore when the job is done
+					<-semaphore
+				}()
+
+				job, err := fetchSolutionFromPool()
+				if err == nil {
+					result := actuator.JudgeUserSubmit(job)
+					job.UpdateResult(result)
+				} else {
+					fmt.Println(err)
+				}
+			}()
+		default:
+			// Maximum concurrency reached, wait before checking again
+			// No new tasks will be fetched until a worker becomes available
 		}
-		time.Sleep(4 * time.Second)
+
+		time.Sleep(500 * time.Millisecond)
 	}
 }
